432ba8804bbe8444544ea38387ec54f766ddab45
SUSANAVARELA <sudava10@gmail.com>
2023-02-07 18:22:00 -0300

    created BookRepositoryTest

---
+++ src/test/java/com/wolox/training/repositories/BookRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.wolox.training.repositories;
+
+import com.wolox.training.models.Book;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@DataJpaTest
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    private Book book;
+
+    @BeforeEach
+    public void setUp(){
+        book =  new Book("Terror", "Stephen King", "abcd", "IT", "Tu tambien flotaras",
+                "Viking", "1986", 1504, "abcd");
+        bookRepository.save(book);
+    }
+
+    @DisplayName("Book: Book saved successfully")
+    @Test
+    void save() {
+        Book savebook =  new Book("Terror", "Stephen King", "abcd", "IT2", "Tu tambien flotaras",
+                "Viking", "1986", 1504, "abcd");
+        bookRepository.save(savebook);
+        long id = 2;
+        Optional<Book> savedBook = bookRepository.findById(id);
+        Assertions.assertEquals(book.getGenre(), savedBook.get().getGenre());
+
+    }
+
+    @DisplayName("Book: Book find by title found successfully")
+    @Test
+    void findByTitle() {
+        Optional<Book> savedBook = bookRepository.findByTitle(book.getTitle());
+        Assertions.assertTrue(savedBook.isPresent());
+        Assertions.assertTrue(savedBook.get().getId() > 0);
+
+    }
+
+    @DisplayName("Book: Book find by id found successfully")
+    @Test
+    void findById(){
+        long id = 1;
+        Optional<Book> savedBook = bookRepository.findById(id);
+        Assertions.assertEquals(book.getGenre(), savedBook.get().getGenre());
+    }
+
+
+}
---
+++ src/test/java/com/wolox/training/repositories/UserRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.wolox.training.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findByUsername() {
+    }
+}
