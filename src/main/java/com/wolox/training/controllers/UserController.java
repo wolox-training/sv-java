c16f71ca273e30eadc07a7bea85c61163fc8c4cd
SUSANAVARELA <sudava10@gmail.com>
2023-02-15 15:50:42 -0300

    delete anotation

--- src/main/java/com/wolox/training/controllers/UserController.java
+++ src/main/java/com/wolox/training/controllers/UserController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +23,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 import java.util.Optional;
 
 @Controller
